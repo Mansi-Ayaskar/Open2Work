88e5d0a820386daec646ecdaa9d433f1c84e1b3e
Neeraj Belsare <belsareneeraj@gmail.com>
2024-02-24 18:33:06 +0530

    Added config.py

---
+++ pythonAPI/config.py
@@ -0,0 +1,14 @@
+class Config:
+    # MySQL configurations
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'root'
+    MYSQL_DB = 'semicolons'
+
+    # Mail configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USERNAME = 'adityadhamale2303@gmail.com'
+    MAIL_PASSWORD = 'nize zfub pcux wkki'
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
