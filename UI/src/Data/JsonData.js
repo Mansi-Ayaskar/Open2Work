60086e6bc4ee0b30a48275c58b37d4262dc889f9
sanjaykumar_gajera <sanjaykumar_gajera@persistent.com>
2024-02-24 21:41:03 +0530

    fix for filter and search

---
+++ UI/src/Data/JsonData.js
@@ -0,0 +1,275 @@
+const JsonData = [
+    {
+        "id": "65da054aaccb5e34125f2fb9",
+        "name": "Woods Evans",
+        "email": "woodsevans@zorromop.com",
+        "currentLocation": "Bawcomville",
+        "preferredLocation": "Worcester",
+        "age": 39,
+        "gender": "male",
+        "skills": [
+            "aute",
+            "elit",
+            "cillum",
+            "ut",
+            "commodo",
+            "occaecat",
+            "sunt"
+        ]
+    },
+    {
+        "id": "65da054aa8c045c77b2308c4",
+        "name": "Kendra Rios",
+        "email": "kendrarios@zorromop.com",
+        "currentLocation": "Northridge",
+        "preferredLocation": "Mayfair",
+        "age": 34,
+        "gender": "female",
+        "skills": [
+            "deserunt",
+            "ut",
+            "anim",
+            "eu",
+            "do",
+            "ipsum",
+            "culpa"
+        ]
+    },
+    {
+        "id": "65da054a282536edad932f1a",
+        "name": "Henrietta Ratliff",
+        "email": "henriettaratliff@zorromop.com",
+        "currentLocation": "Rivereno",
+        "preferredLocation": "Waverly",
+        "age": 38,
+        "gender": "female",
+        "skills": [
+            "minim",
+            "sit",
+            "sit",
+            "qui",
+            "laborum",
+            "eiusmod",
+            "ullamco"
+        ]
+    },
+    {
+        "id": "65da054ab0471aeeedbe52f3",
+        "name": "Mccall Francis",
+        "email": "mccallfrancis@zorromop.com",
+        "currentLocation": "Grenelefe",
+        "preferredLocation": "Sunnyside",
+        "age": 27,
+        "gender": "male",
+        "skills": [
+            "cillum",
+            "culpa",
+            "sint",
+            "enim",
+            "est",
+            "labore",
+            "voluptate"
+        ]
+    },
+    {
+        "id": "65da054a3f88304e039ea67f",
+        "name": "Lou Shaffer",
+        "email": "loushaffer@zorromop.com",
+        "currentLocation": "Coventry",
+        "preferredLocation": "Konterra",
+        "age": 34,
+        "gender": "female",
+        "skills": [
+            "enim",
+            "aliqua",
+            "incididunt",
+            "ad",
+            "dolor",
+            "aliqua",
+            "voluptate"
+        ]
+    },
+    {
+        "id": "65da054a365c30790ee457e5",
+        "name": "Iris Collins",
+        "email": "iriscollins@zorromop.com",
+        "currentLocation": "Websterville",
+        "preferredLocation": "Waukeenah",
+        "age": 29,
+        "gender": "female",
+        "skills": [
+            "sit",
+            "aliqua",
+            "esse",
+            "anim",
+            "voluptate",
+            "irure",
+            "do"
+        ]
+    },
+    {
+        "id": "65da054ac0b4fba66520c16c",
+        "name": "Briggs Roberts",
+        "email": "briggsroberts@zorromop.com",
+        "currentLocation": "Nanafalia",
+        "preferredLocation": "Spokane",
+        "age": 22,
+        "gender": "male",
+        "skills": [
+            "fugiat",
+            "laborum",
+            "exercitation",
+            "ex",
+            "laboris",
+            "aliquip",
+            "sunt"
+        ]
+    },
+    {
+        "id": "65da054a8e3edc268a05f3da",
+        "name": "Freda Mullen",
+        "email": "fredamullen@zorromop.com",
+        "currentLocation": "Ronco",
+        "preferredLocation": "Berlin",
+        "age": 21,
+        "gender": "female",
+        "skills": [
+            "ad",
+            "pariatur",
+            "excepteur",
+            "voluptate",
+            "deserunt",
+            "qui",
+            "tempor"
+        ]
+    },
+    {
+        "id": "65da054a61eef55b31399b5b",
+        "name": "Rocha Bridges",
+        "email": "rochabridges@zorromop.com",
+        "currentLocation": "Hailesboro",
+        "preferredLocation": "Rehrersburg",
+        "age": 21,
+        "gender": "male",
+        "skills": [
+            "aliqua",
+            "laborum",
+            "deserunt",
+            "adipisicing",
+            "exercitation",
+            "consequat",
+            "reprehenderit"
+        ]
+    },
+    {
+        "id": "65da054ad85c848bf38a31d0",
+        "name": "Pearl Rivas",
+        "email": "pearlrivas@zorromop.com",
+        "currentLocation": "Gilmore",
+        "preferredLocation": "Malo",
+        "age": 27,
+        "gender": "female",
+        "skills": [
+            "qui",
+            "do",
+            "fugiat",
+            "cupidatat",
+            "laborum",
+            "sunt",
+            "amet"
+        ]
+    },
+    {
+        "id": "65da054afbd0f8b9eebcc377",
+        "name": "Suzette Hendrix",
+        "email": "suzettehendrix@zorromop.com",
+        "currentLocation": "Linwood",
+        "preferredLocation": "Witmer",
+        "age": 30,
+        "gender": "female",
+        "skills": [
+            "consectetur",
+            "eu",
+            "mollit",
+            "culpa",
+            "officia",
+            "ad",
+            "sit"
+        ]
+    },
+    {
+        "id": "65da054a292b515f8f63c5d8",
+        "name": "Navarro Schwartz",
+        "email": "navarroschwartz@zorromop.com",
+        "currentLocation": "Cressey",
+        "preferredLocation": "Accoville",
+        "age": 40,
+        "gender": "male",
+        "skills": [
+            "quis",
+            "sit",
+            "deserunt",
+            "exercitation",
+            "sit",
+            "duis",
+            "cupidatat"
+        ]
+    },
+    {
+        "id": "65da054af8de24498a2040d0",
+        "name": "Farmer Le",
+        "email": "farmerle@zorromop.com",
+        "currentLocation": "Sultana",
+        "preferredLocation": "Buxton",
+        "age": 24,
+        "gender": "male",
+        "skills": [
+            "quis",
+            "deserunt",
+            "aute",
+            "occaecat",
+            "Lorem",
+            "mollit",
+            "proident"
+        ]
+    },
+    {
+        "id": "65da054a31fa9ec3a14921ff",
+        "name": "Cox Parker",
+        "email": "coxparker@zorromop.com",
+        "currentLocation": "Harborton",
+        "preferredLocation": "Hanover",
+        "age": 35,
+        "gender": "male",
+        "skills": [
+            "sit",
+            "consequat",
+            "veniam",
+            "tempor",
+            "ullamco",
+            "et",
+            "proident"
+        ]
+    },
+    {
+        "id": "65da054a5f4b409761a9ceb1",
+        "name": "Audra Carpenter",
+        "email": "audracarpenter@zorromop.com",
+        "currentLocation": "Barclay",
+        "preferredLocation": "Unionville",
+        "age": 39,
+        "gender": "female",
+        "skills": [
+            "consectetur",
+            "cupidatat",
+            "ex",
+            "fugiat",
+            "nulla",
+            "aute",
+            "esse"
+        ]
+    }
+];
+
+
+export default JsonData;
