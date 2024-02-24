1c76463240aba400e4d9d9b0c791d0183d55af7f
chintan-persistent <chintan_ramani@persistent.com>
2024-02-24 14:54:02 +0530

    Added Routes with Profile page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
 import Dashboard from './Pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Profile from './Pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <Dashboard/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="profile" element={<Profile />} />
+        <Route
+          path="*"
+          element={
+            <div style={{ padding: 20 }}>
+              <h2>404: Page Not Found</h2>
+            </div>
+          }
+        />
+      </Routes>
+    </Router>
+    // <div className="App">
+
+    // </div>
   );
 }
 
