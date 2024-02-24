6beee5fbd302999474e140aca50c8e5c3b96c8e6
Mansi-Ayaskar <mansi_ayaskar@persistent.com>
2024-02-25 01:44:32 +0530

    form-css

--- UI/src/Components/CustomSelect.js
+++ UI/src/Components/CustomSelect.js
@@ -1,20 +1,20 @@
-import { Select, MenuItem, OutlinedInput } from '@mui/material';
+import { Select, MenuItem } from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import '../Styles/CustomSelect.css';
 
-const CustomSelect = ({ name, value, onChange, items, isMulti }) => {
+const CustomSelect = ({ label, name, value, onChange, items, isMulti }) => {
   return (
-    <Select
-      input={<OutlinedInput label="Multiple Select" />}
-      name={name}
-      value={value}
-      onChange={onChange}
-      multiple={isMulti}
-    >
-      {items.map((item) => (
-        <MenuItem key={item} value={item}>
-          {item}
-        </MenuItem>
-      ))}
-    </Select>
+    <FormControl variant="standard" className="customSelect">
+      <InputLabel id="demo-simple-select-standard-label">{label}</InputLabel>
+      <Select name={name} value={value} onChange={onChange} multiple={isMulti}>
+        {items.map((item) => (
+          <MenuItem key={item} value={item}>
+            {item}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
   );
 };
 
